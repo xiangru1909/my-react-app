9dfd7e00340c3e6fcfc269678e80c1d9aee67854
xiangru1909 <124892244+xiangru1909@users.noreply.github.com>
2023-05-20 06:45:55 +0800

    更新 setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,39 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+
+import React from 'react';
+
+class RotatingT extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { rotation: 0 };
+  }
+
+  componentDidMount() {
+    this.rotateT();
+  }
+
+  rotateT() {
+    setInterval(() => {
+      this.setState(prevState => ({ rotation: prevState.rotation + 10 }));
+    }, 100);
+  }
+
+  render() {
+    const { rotation } = this.state;
+
+    const style = {
+      transform: `rotate(${rotation}deg)`,
+    };
+
+    return (
+      <div style={style}>
+        <h1>Rotating T</h1>
+        <div className="rotating-t">
+          <div className="t-horizontal"></div>
+          <div className="t-vertical"></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default RotatingT;
